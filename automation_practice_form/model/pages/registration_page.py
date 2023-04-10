a62b646a45d5cd00457cffefad3aa8f8417e7795
Aleksandr Kozelskikh <a.n.kozelskikh@gmail.com>
2023-04-10 22:55:13 +0300

    HW 11.
    Ready for Jenkins

---
+++ automation_practice_form/model/pages/registration_page.py
@@ -0,0 +1,111 @@
+import os
+
+from selene import have, command
+#from selene.support.shared import browser
+
+
+class RegistrationPage:
+    def __init__(self, setup_browser):
+        self.browser = setup_browser
+        self.first_name = self.browser.element('#firstName')
+        self.last_name = self.browser.element('#lastName')
+        self.email = self.browser.element('#userEmail')
+        self.gender = self.browser.element('[for="gender-radio-1"]')
+        self.user_number = self.browser.element('#userNumber')
+        self.date_of_birth_input = self.browser.element('#dateOfBirthInput')
+        self.date_of_birth_year = self.browser.element('.react-datepicker__year-select')
+        self.date_of_birth_month = self.browser.element('.react-datepicker__month-select')
+        self.subjects_input = self.browser.element('#subjectsInput')
+        self.hobbies = self.browser.element('[for="hobbies-checkbox-2"]')
+        self.upload_picture = self.browser.element('#uploadPicture')
+        self.address = self.browser.element('#currentAddress')
+        self.state = self.browser.element('#state')
+        self.state_select = self.browser.element('#react-select-3-option-0')
+        self.city = self.browser.element('#city')
+        self.city_select = self.browser.element('#react-select-4-option-0')
+        self.submit = self.browser.element('#submit')
+
+    def open(self):
+        self.browser.open('http://demoqa.com/automation-practice-form')
+        self.browser.config.driver.maximize_window()
+        self.browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
+            have.size_greater_than_or_equal(3)
+        )
+        self.browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
+        return self
+
+    def fill_first_name(self, value):
+        self.first_name.type(value)
+        return self
+
+    def fill_last_name(self, value):
+        self.last_name.type(value)
+        return self
+
+    def fill_email(self, value):
+        self.email.type(value)
+        return self
+
+    def select_gender(self, value):
+        self.gender.should(have.text(f'{value}')).click()
+        return self
+
+    def fill_mobile_phone(self, value):
+        self.user_number.type(value)
+        return self
+
+    def fill_date_of_birth(self, year, month, day):
+        self.date_of_birth_input.click()
+        self.date_of_birth_month.type(month)
+        self.date_of_birth_year.type(year)
+        self.browser.element(
+            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+        ).click()
+        return self
+
+    def select_hobbies(self, value):
+        self.hobbies.should(have.text(value)).click()
+        return self
+
+    def fill_subjects(self, value):
+        self.subjects_input.type(f'{value}').press_enter()
+        return self
+
+    def upload_file(self, value):
+        self.upload_picture.send_keys(os.getcwd() + value)
+        return self
+
+    def fill_address(self, value):
+        self.address.type(value)
+
+    def select_state(self, value):
+        self.state.click()
+        self.state_select.should(have.text(f'{value}')).click()
+        return self
+
+    def select_city(self, value):
+        self.city.click()
+        self.city_select.should(have.text(f'{value}')).click()
+        return self
+
+    def submit_click(self):
+        self.submit.click()
+        return self
+
+    def should_registered_user_with(self, full_name, email, gender, number, dateofbirth, subjects, hobbies, photo,
+                                    address, stateandcity):
+        self.browser.element('.table').all('td').even.should(
+            have.exact_texts(
+                full_name,
+                email,
+                gender,
+                number,
+                dateofbirth,
+                subjects,
+                hobbies,
+                photo,
+                address,
+                stateandcity,
+            )
+        )
+        return self
---
+++ tests/test_registration_user.py
@@ -0,0 +1,44 @@
+import allure
+
+from automation_practice_form.model.pages.registration_page import RegistrationPage
+
+
+def test_registration_form(setup_browser):
+    # GIVEN
+    with allure.step('Открыть главную страницу github.com'):
+        registration_page = RegistrationPage(setup_browser)
+        registration_page.open()
+
+    # WHEN
+    with allure.step('Заполнить поле first name'):
+        registration_page.fill_first_name('yashaka')
+    with allure.step('Заполнить поле last name'):
+        registration_page.fill_last_name('selene')
+    with allure.step('Заполнить поле email'):
+        registration_page.fill_email('yashaka@selene.com')
+    with allure.step('Выбрать значение gender'):
+        registration_page.select_gender('Male')
+    with allure.step('Заполнить поле mobile phone'):
+        registration_page.fill_mobile_phone('9876543210')
+    with allure.step('Заполнить поле date of birt'):
+        registration_page.fill_date_of_birth('2005', 'April', '18')
+    with allure.step('Выбрать значение hobbies'):
+        registration_page.select_hobbies('Reading')
+    with allure.step('Заполнить поле subjects'):
+        registration_page.fill_subjects('Arts')
+    with allure.step('Загрузить файл в поле upload file'):
+        registration_page.upload_file('\\resources\\1.png')
+    with allure.step('Заполнить поле address'):
+        registration_page.fill_address('Address')
+    with allure.step('ЗВыбрать значение state'):
+        registration_page.select_state('NCR')
+    with allure.step('Выбрать значение city'):
+        registration_page.select_city('Delhi')
+    with allure.step('Отправить заполненную форму'):
+        registration_page.submit_click()
+
+    # THEN
+    with allure.step('Пользователь зарегистрирован. Данные верны'):
+        registration_page.should_registered_user_with('yashaka selene', 'yashaka@selene.com', 'Male', '9876543210',
+                                                      '18 April,2005', 'Arts', 'Reading', '1.png',
+                                                      'Address', 'NCR Delhi')
